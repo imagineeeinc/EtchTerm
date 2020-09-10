bc5277a3b000b751f86134c00e81f8a77de715e5
Imagineeeinc <imagineeelego@gmail.com>
2020-09-10 20:19:26 +0100

    Updated docs
    Signed-off-by: Imagineeeinc <imagineeelego@gmail.com>

--- Docs/app.js
+++ Docs/app.js
@@ -1,4 +1,11 @@
 setInterval(function() {
     if (cijs("sizeInW") > 679)
-    document.getElementById("body").style.width = cijs("sizeInW") - 300 + "px"
+    document.getElementById("body").style.width = cijs("sizeInW") - 350 + "px"
 }, 1)
+document.getElementById("menu").onclick = function() {
+    if (document.getElementById("index").className == "hmenu") {
+        document.getElementById("index").className = "smenu";
+    } else if (document.getElementById("index").className == "smenu") {
+        document.getElementById("index").className = "hmenu";
+    }
+}
